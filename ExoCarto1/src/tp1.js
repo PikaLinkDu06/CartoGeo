bcb4c1c3fce2f18c475e1dc44c95350483a43a41
Julien <jajg@laposte.net>
2021-01-06 10:29:06 +0100

    Continuage du TP

--- ExoCarto1/src/tp1.js
+++ ExoCarto1/src/tp1.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
 
+  let exercice2 = $('#exercice2') ;
+  let exercice3 = $('#exercice3') ;
+
   navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position) ;
-    $('#function1').find('ul').append(
-      '<li>Longitude : ' + position.coords.longitude + '</li>',
+    var firstDiv = exercice2.find('.function1') ;
+    firstDiv.find('ul').append(
+      "<li>Longitude : " + position.coords.longitude + "</li>",
       "<li>Latitude : " + position.coords.latitude + "</li>",
       "<li>Precision : " + position.coords.accuracy + "</li>",
       "<li>Vitesse : " + position.coords.speed + "</li>",
@@ -12,13 +15,36 @@ $(document).ready(function() {
   });
 
   navigator.geolocation.watchPosition(function(position) {
-    $('#function2').find('ul').empty() ;
-    $('#function2').find('ul').append(
+    var secondDiv = exercice2.find('.function2') ;
+    secondDiv.find('ul').empty() ;
+    secondDiv.find('ul').append(
       '<li>Longitude : ' + position.coords.longitude + '</li>',
       "<li>Latitude : " + position.coords.latitude + "</li>",
       "<li>Precision : " + position.coords.accuracy + "</li>",
       "<li>Vitesse : " + position.coords.speed + "</li>",
       "<li>Timestamp : " + position.timestamp + "</li>"
+    ) ;
+  });
+
+  window.addEventListener('deviceorientation', function(orientation) {
+    var firstDiv = exercice3.find('div') ;
+    firstDiv.find('ul').append(
+      "<li>Alpha : " + orientation.alpha + "</li>",
+      "<li>Beta : " + orientation.beta + "</li>",
+      "<li>Gamma : " + orientation.gamma + "</li>"
+    ) ;
+  });
+
+  window.addEventListener('devicemotion', function(orientation) {
+    var firstDiv = exercice3.find('div') ;
+    var firstDiv = exercice3.find('div') ;
+    firstDiv.find('ul').append(
+      "<li>Acceleration X : " + orientation.acceleration.x + "</li>",
+      "<li>Acceleration Y : " + orientation.acceleration.y + "</li>",
+      "<li>Acceleration Z : " + orientation.acceleration.z + "</li>",
+      "<li>Rotation alpha : " + orientation.rotationRate.alpha + "</li>",
+      "<li>Rotation beta : " + orientation.rotationRate.beta + "</li>",
+      "<li>Rotation gamma : " + orientation.rotationRate.gamma + "</li>"
     ) ;
   });
 
