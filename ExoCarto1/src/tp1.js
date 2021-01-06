6d335ccc008e8281906cacd6d3a8a82c52cc0ddc
Julien <jajg@laposte.net>
2021-01-06 10:41:30 +0100

    No rage de mon continuage

--- ExoCarto1/src/tp1.js
+++ ExoCarto1/src/tp1.js
@@ -26,26 +26,30 @@ $(document).ready(function() {
     ) ;
   });
 
-  window.addEventListener('deviceorientation', function(orientation) {
-    var firstDiv = exercice3.find('div') ;
-    firstDiv.find('ul').append(
-      "<li>Alpha : " + orientation.alpha + "</li>",
-      "<li>Beta : " + orientation.beta + "</li>",
-      "<li>Gamma : " + orientation.gamma + "</li>"
-    ) ;
-  });
+  if(window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function(orientation) {
+      var firstDiv = exercice3.find('.function1') ;
+      firstDiv.find('ul').append(
+        "<li>Alpha : " + orientation.alpha + "</li>",
+        "<li>Beta : " + orientation.beta + "</li>",
+        "<li>Gamma : " + orientation.gamma + "</li>"
+      ) ;
+    });
+  }
 
-  window.addEventListener('devicemotion', function(orientation) {
-    var firstDiv = exercice3.find('div') ;
-    var firstDiv = exercice3.find('div') ;
-    firstDiv.find('ul').append(
-      "<li>Acceleration X : " + orientation.acceleration.x + "</li>",
-      "<li>Acceleration Y : " + orientation.acceleration.y + "</li>",
-      "<li>Acceleration Z : " + orientation.acceleration.z + "</li>",
-      "<li>Rotation alpha : " + orientation.rotationRate.alpha + "</li>",
-      "<li>Rotation beta : " + orientation.rotationRate.beta + "</li>",
-      "<li>Rotation gamma : " + orientation.rotationRate.gamma + "</li>"
-    ) ;
-  });
+  if(window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', function(orientation) {
+      var secondDiv = exercice3.find('.function2') ;
+      secondDiv.find('ul').empty() ;
+      secondDiv.find('ul').append(
+        "<li>Acceleration X : " + orientation.acceleration.x + "</li>",
+        "<li>Acceleration Y : " + orientation.acceleration.y + "</li>",
+        "<li>Acceleration Z : " + orientation.acceleration.z + "</li>",
+        "<li>Rotation alpha : " + orientation.rotationRate.alpha + "</li>",
+        "<li>Rotation beta : " + orientation.rotationRate.beta + "</li>",
+        "<li>Rotation gamma : " + orientation.rotationRate.gamma + "</li>"
+      ) ;
+    });
+  }
 
 }) ;
