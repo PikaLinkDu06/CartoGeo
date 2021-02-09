9041ae5cddd76d0fab50cd34dcbff488213a32ea
Julien <jajg@laposte.net>
2021-02-09 08:58:24 +0100

    OUI

--- MiniProjet/src/canvas.js
+++ MiniProjet/src/canvas.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
 
   var canvas = document.getElementById('compassCanvas') ;
-  var ctx = canvas.getContext('2d');
+  var ctx1 = canvas.getContext('2d');
+  var ctx2 = canvas.getContext('2d') ;
   var boussole = new Image();
   var aiguille = new Image() ;
   boussole.src = './images/compass.png' ;
   aiguille.src = './images/needle.png' ;
   boussole.onload = function() {
-    aiguille.onload = function() {
-      ctx.drawImage(aiguille, 125, 125, 125, 125) ;
-    }
-    ctx.drawImage(boussole, 125, 125, 125, 125) ;
+    ctx1.drawImage(boussole, 125, 125, 125, 125) ;
+  }
+  aiguille.onload = function() {
+    ctx2.drawImage(aiguille, 125, 125, 125, 125) ;
   }
 
   if(window.DeviceOrientationEvent) {
     window.addEventListener('devicemotion', function(orientation) {
-      ctx.rotate(orientation.rotationRate.alpha * Math.PI / 180) ;
+      ctx2.rotate(orientation.rotationRate.alpha * Math.PI / 180) ;
     });
   }
 
