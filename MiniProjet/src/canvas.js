ed462389c57b1632aa173b1da232b7394b2b2169
Julien <jajg@laposte.net>
2021-02-09 09:54:31 +0100

    f

--- MiniProjet/src/canvas.js
+++ MiniProjet/src/canvas.js
@@ -6,25 +6,22 @@ $(document).ready(function() {
   var aiguille = new Image() ;
   boussole.src = './images/compass.png' ;
   aiguille.src = './images/needle.png' ;
-  /*
-  boussole.onload = function() {
-  ctx.drawImage(boussole, 25, 25) ;
-}
-aiguille.onload = function() {
-ctx.drawImage(aiguille, 25, 25) ;
-} */
 
-if(window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', function(orientation) {
-    console.log(orientation) ;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(boussole, 25, 25);
-    ctx.save();
-    ctx.translate(50, 50) ;
-    ctx.rotate(orientation.alpha * (Math.PI / 180));
-    ctx.drawImage(aiguille, -25, -25);
-    ctx.restore() ;
-  });
-}
+  if(window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function(orientation) {
+      console.log(orientation) ;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      ctx.drawImage(boussole, 0, 0);
+      ctx.save();
+
+      ctx.translate(25, 25) ;
+      ctx.rotate(orientation.alpha * (Math.PI / 180));
+
+      ctx.drawImage(aiguille, -25, -25);
+
+      ctx.restore() ;
+    });
+  }
 
 }) ;
