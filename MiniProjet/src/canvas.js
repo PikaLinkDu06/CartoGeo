afba20ba1c0cf07e50749f322458c5c494f7b877
Julien <jajg@laposte.net>
2021-02-09 09:11:21 +0100

    OUI

--- MiniProjet/src/canvas.js
+++ MiniProjet/src/canvas.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
 
   var canvas = document.getElementById('compassCanvas') ;
-  var ctx1 = canvas.getContext('2d');
-  var ctx2 = canvas.getContext('2d') ;
+  var ctx = canvas.getContext('2d');
   var boussole = new Image();
   var aiguille = new Image() ;
   boussole.src = './images/compass.png' ;
   aiguille.src = './images/needle.png' ;
   boussole.onload = function() {
-    ctx1.drawImage(boussole, 125, 125, 125, 125) ;
+    ctx.drawImage(boussole, 125, 125, 125, 125) ;
   }
   aiguille.onload = function() {
-    ctx2.drawImage(aiguille, 125, 125, 125, 125) ;
+    ctx.drawImage(aiguille, 125, 125, 125, 125) ;
   }
 
-  if(window.DeviceOrientationEvent) {
+  if(window.DeviceMotionEvent) {
     window.addEventListener('devicemotion', function(orientation) {
-      ctx2.rotate(orientation.rotationRate.alpha * Math.PI / 180) ;
+      console.log(orientation) ;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(boussole, 0, 0);
+      ctx.save();
+      ctx.translate(100, 100);
+      ctx.rotate(orientation.rotationRate.alpha * (Math.PI / 180));
+      ctx.drawImage(aiguille, -100, -100);
+      ctx.restore();
     });
   }
 
