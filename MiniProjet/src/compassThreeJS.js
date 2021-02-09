e2f64dafd0644e4b3ad51b4c1a8da116c1cca648
Julien <jajg@laposte.net>
2021-02-09 12:17:52 +0100

    h

--- MiniProjet/src/compassThreeJS.js
+++ MiniProjet/src/compassThreeJS.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera( 25, 1, 0.1, 1000 );
-  camera.position.z = 6 ;
+  camera.position.set(0, 8, 0) ;
 
   const renderer = new THREE.WebGLRenderer({ canvas: threeJSCompassCanvas });
   renderer.setSize(500, 500);
@@ -10,11 +10,12 @@ $(document).ready(function() {
   const light = new THREE.AmbientLight( 0xffffff );
   scene.add( light );
 
-  const controls = new THREE.OrbitControls( camera, renderer.domElement );
-
+  let duck ;
   const gltfLoader = new THREE.GLTFLoader();
   gltfLoader.load('./models/Duck.gltf', function(gltf) {
-    scene.add( gltf.scene ) ;
+    duck = gltf.scene ;
+    duck.rotation.set(0, (90 * (Math.PI / 180)), 0) ;
+    scene.add( duck ) ;
   },
   function ( xhr ) {
     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -25,8 +26,18 @@ $(document).ready(function() {
   }
 ) ;
 
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+
 const animate = function () {
+  
   requestAnimationFrame( animate );
+
+  if(window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function(orientation) {
+      if(duck !== undefined) duck.rotation.set(0, orientation.alpha * (Math.PI / 180), 0) ; ;
+    }) ;
+  }
+
   renderer.render( scene, camera );
 };
 animate();
